ea6cf3527a2fe3e484e91603ea53ceec43f3e1d7
JOS-RE <joshithreddy.g@gmail.com>
2022-01-11 19:04:52 +0530

    ⭕ reversing a string

---
+++ Day 2/Reverse a string/reverse.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+a = input('Enter string : ')
+print('\nchecking the string...')
+sleep(2)
+print('\nreversing the string...')
+sleep(2)
+print('\nReversing the string complete...')
+sleep(2)
+print('\noh wait ... where is it')
+sleep(2)
+print(f'Here you go : {a[::-1]}')
