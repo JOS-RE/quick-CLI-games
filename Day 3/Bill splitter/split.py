f763465a0cf785fd58fb01ad472b89e60fa727e6
JOS-RE <joshithreddy.g@gmail.com>
2022-01-12 20:03:13 +0530

    ⭕ Bill splitter app

---
+++ Day 3/Bill splitter/split.py
@@ -0,0 +1,6 @@
+# this is a very very very simple bill splitter
+
+price = int(input('Enter the total price : '))
+num = int(input('Enter total number of people : '))
+
+print(f'Each person has to pay {price/num} dollars')
